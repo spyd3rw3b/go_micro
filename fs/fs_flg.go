137d6ee5c63c5f0e7dbca42ae97976f1cf21b8bb
spyd3rw3b <spyd3rw3b@gmail.com>
2016-12-21 19:04:59 -0600

    fileServer http with flags

---
+++ fs/fs_flg.go
@@ -0,0 +1,23 @@
+package main 
+
+import (
+       "flag"
+       "net/http"
+        "os")
+
+
+func main() {
+
+  var dir string
+  port := flag.String("port", "3000", "port to serve HTTP")
+  path := flag.String("path", "", "path to serve")
+  flag.Parse()
+
+  if *path == "" {
+       dir, _ = os.Getwd()
+    }else{
+       dir = *path
+  }
+
+  http.ListenAndServe(":"+*port, http.FileServer(http.Dir(dir)))
+}
